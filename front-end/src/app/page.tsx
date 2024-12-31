ab4ef6d84d704ba3a16a4857227ca95183b59dd3
Ahmed Hariri <Ahmedhariri58@gmail.com>
2024-12-31 17:22:03 +0100

    Create Section Postes

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,13 +1,12 @@
+import Postes from "@/components/postes"
 import Profile from "@/components/profile"
 
 export default function Home() {
     return <>
         <section className="w-full h-full flex justify-center">
-            <div className="w-full lg:max-w-[1200px] h-full flex justify-between flex-wrap lg:flex-nowrap gap-2 p-4 lg:py-4 xl:px-0 lg:pt-2">
+            <div className="w-full lg:max-w-[1200px] h-full flex justify-between flex-wrap lg:flex-nowrap gap-5 p-4 lg:py-4 xl:px-0 lg:pt-2">
                 <Profile />
-                <div className="w-[50%] h-full bg-yellow-500">
-
-                </div>
+                <Postes />
                 <div className="w-[25%] h-full bg-green-500">
 
                 </div>
